b0cd023bc7069d46e2d6a9b71c1d5b7fe02d8ebb
tranquilo <givemytaxesback@gmail.com>
2017-10-19 05:26:43 +0800

    extended templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
@@ -10,3 +10,7 @@ from django.utils import timezone
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
